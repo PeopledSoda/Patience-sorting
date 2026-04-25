dfc4ca6200c4de0fcd46b21d3956d21005bf8871
Idiatov <idiatov2007@mail.ru>
2026-04-25 16:57:12 +0300

    создание данных

---
+++ src/main/java/GenerateData.java
@@ -0,0 +1,25 @@
+import java.io.FileWriter;
+import java.util.Random;
+import java.io.IOException;
+
+public class GenerateData {
+    public static void main(String[] args) throws IOException{
+        Random rand = new Random();
+
+        int datasets = 70;
+        for (int i = 0; i <= datasets;i++){
+            int size = 100 + rand.nextInt(9901);
+
+            FileWriter writer = new FileWriter("data_"+i+".txt");
+
+            for (int j = 0; j <size;j++){
+                int num = rand.nextInt(100000);
+
+                writer.write(num+"\n");
+            }
+            writer.close();
+
+        }
+        System.out.println("Данные сгенерированы");
+    }
+}
