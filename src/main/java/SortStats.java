3e3baea8b74715317ba1546b0f848b9205f29639
Idiatov <idiatov2007@mail.ru>
2026-04-25 17:36:22 +0300

    вывод времени и итераций

---
+++ src/main/java/SortStats.java
@@ -0,0 +1,10 @@
+public class SortStats {
+    long time;
+    long operations;
+
+    public SortStats(long time, long operations){
+        this.time = time;
+        this.operations = operations;
+
+    }
+}
